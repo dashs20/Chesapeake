266bd0a3aafa7f26bac7ea8db24aefdef0b7362b
Dash <143964362+dashs20@users.noreply.github.com>
2026-06-28 14:32:09 -0700

    pin map for GOOFC AIO

--- src/pin_config/pin_config.cpp
+++ src/pin_config/pin_config.cpp
@@ -5,12 +5,12 @@ const int ELRS_RX_PIN = D7; // UART 0 RX (GPIO 1)
 const int ELRS_TX_PIN = D6; // UART 0 TX (GPIO 0)
 
 // ESC Pins (D0/D1)
-const int ESC_1_PIN = D0; // PWM Slice 5A (GPIO 26)
-const int ESC_2_PIN = D1; // PWM Slice 5B (GPIO 27)
+const int ESC_1_PIN = D1;
+const int ESC_2_PIN = D2;
 
 // Servo Pins (D4/D5)
-const int SERVO_1_PIN = D4; // PWM Slice 3A (GPIO 6)
-const int SERVO_2_PIN = D5; // PWM Slice 3B (GPIO 7)
+const int SERVO_1_PIN = D8;
+const int SERVO_2_PIN = D9;
 
 // IMU Pins
-const int CS_PIN = D11;
+const int CS_PIN = D18;
